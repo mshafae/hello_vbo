9ce0781a0d97d0c38afc5c813b67124aaea96eae
Michael Shafae <mshafae@fullerton.edu>
2024-03-21 11:47:12 -0700

    Cleaned up; added animation.

--- hello_vbo.cc
+++ hello_vbo.cc
@@ -6,12 +6,10 @@
 #include <cstddef>
 #include <cstdio>
 #include <cstdlib>
-
 #include <glm/ext/matrix_clip_space.hpp>
 #include <glm/ext/matrix_transform.hpp>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
-
 #include <iostream>
 
 #define GLFW_INCLUDE_GLU
@@ -22,8 +20,12 @@ typedef struct {
   glm::vec3 color;
 } vertex_t;
 
-const char* vertex_shader =
-    "#version 400\n"
+bool rotate_on;
+GLuint vertex_array_object_id;
+GLuint shader_program_id;
+
+const char* vertex_shader_src =
+    "#version 410\n"
     "uniform mat4 model_view_matrix;"
     "uniform mat4 projection_matrix;"
     "in vec3 in_vertex;"
@@ -37,8 +39,8 @@ const char* vertex_shader =
     "  v_out_color = vec4(in_color, 1.0);"
     "}";
 
-const char* fragment_shader =
-    "#version 400\n"
+const char* fragment_shader_src =
+    "#version 410\n"
     // Use the same name as what was used in the vertex shader
     "in vec4 v_out_color;"
     "out vec4 frag_color;"
@@ -48,9 +50,9 @@ const char* fragment_shader =
     "  frag_color = v_out_color;"
     "}";
 
-#define MSGLError() _MSGLError(stderr, __FILE__, __LINE__)
+#define msglError() _msglError(stderr, __FILE__, __LINE__)
 
-bool _MSGLError(FILE* out, const char* filename, int line) {
+bool _msglError(FILE* out, const char* filename, int line) {
   bool ret = false;
   GLenum err = glGetError();
   while (err != GL_NO_ERROR) {
@@ -62,7 +64,7 @@ bool _MSGLError(FILE* out, const char* filename, int line) {
   return (ret);
 }
 
-void MSGLVersion() {
+void msglVersion() {
   fprintf(stderr, "Vendor: %s\n", glGetString(GL_VENDOR));
   fprintf(stderr, "Renderer: %s\n", glGetString(GL_RENDERER));
   fprintf(stderr, "OpenGL Version: %s\n", glGetString(GL_VERSION));
@@ -70,16 +72,21 @@ void MSGLVersion() {
           glGetString(GL_SHADING_LANGUAGE_VERSION));
 }
 
-static void ErrorCallback(int error, const char* description) {
+void Init() { rotate_on = false; }
+
+static void msglfwErrorCallback(int error, const char* description) {
   fprintf(stderr, "GLFW Error: %s\n", description);
 }
 
-static void KeyCallback(GLFWwindow* window, int key, int scancode, int action,
-                        int mods) {
+static void msglfwKeyCallback(GLFWwindow* window, int key, int scancode,
+                              int action, int mods) {
   if (action == GLFW_PRESS) {
     switch (key) {
       case GLFW_KEY_ESCAPE:
         glfwSetWindowShouldClose(window, GL_TRUE);
+        break;
+      case GLFW_KEY_R:
+        rotate_on = !rotate_on;
         break;
       default:
         fprintf(stderr, "Key pressed: %c:%d\n", key, key);
@@ -88,8 +95,8 @@ static void KeyCallback(GLFWwindow* window, int key, int scancode, int action,
   }
 }
 
-static void MouseButtonCallback(GLFWwindow* window, int button, int action,
-                                int mods) {
+static void msglfwMouseButtonCallback(GLFWwindow* window, int button,
+                                      int action, int mods) {
   if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS) {
     fprintf(stderr, "Right mouse button pressed.\n");
   } else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
@@ -97,48 +104,71 @@ static void MouseButtonCallback(GLFWwindow* window, int button, int action,
   }
 }
 
-// static void MouseCursorPositionCallback(GLFWwindow* window, double xpos,
-// double ypos) {
-//   fprintf(stderr, "Mouse at (%f, %f)\n", xpos, ypos);
-// }
-//
-// static void CursorEnteredCallback(GLFWwindow* window, int entered){
-//   if (entered == GLFW_TRUE) {
-//     fprintf(stderr, "Cursor entered the window.\n");
-//   } else {
-//     fprintf(stderr, "Cursor exited the window.\n");
-//   }
-// }
-
-// void DrawTriangle() {
-GLuint vao;
-GLuint shader_program;
-
 void InitShader() {
   // The source code of the shader programs are defined as
   // const char* variables at the top of the file.
 
+  GLint compiled_ok;
+
   // Create a vertex shader
-  GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-  MSGLError();
-  glShaderSource(vs, 1, &vertex_shader, NULL);
-  glCompileShader(vs);
-  MSGLError();
+  GLuint vertex_shader_id = glCreateShader(GL_VERTEX_SHADER);
+  msglError();
+  glShaderSource(vertex_shader_id, 1, &vertex_shader_src, NULL);
+  glCompileShader(vertex_shader_id);
+  glGetShaderiv(vertex_shader_id, GL_COMPILE_STATUS, &compiled_ok);
+  msglError();
+  if (!compiled_ok) {
+    fprintf(stderr, "Compilation failed for shader \"%s\"\n",
+            "built-in vertex shader");
+    GLint info_log_length;
+    glGetShaderiv(vertex_shader_id, GL_INFO_LOG_LENGTH, &info_log_length);
+    char* info_log = (char*)malloc(info_log_length);
+    glGetShaderInfoLog(vertex_shader_id, info_log_length, NULL, info_log);
+    fprintf(stderr, "Info Log:\n%s\n", info_log);
+    free(info_log);
+    assert(compiled_ok);
+  }
 
   // Create a fragment shader
-  GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-  MSGLError();
-  glShaderSource(fs, 1, &fragment_shader, NULL);
-  glCompileShader(fs);
-  MSGLError();
+  GLuint fragment_shader_id = glCreateShader(GL_FRAGMENT_SHADER);
+  msglError();
+  glShaderSource(fragment_shader_id, 1, &fragment_shader_src, NULL);
+  glCompileShader(fragment_shader_id);
+  glGetShaderiv(fragment_shader_id, GL_COMPILE_STATUS, &compiled_ok);
+  msglError();
+  if (!compiled_ok) {
+    fprintf(stderr, "Compilation failed for shader \"%s\"\n",
+            "built-in vertex shader");
+    GLint info_log_length;
+    glGetShaderiv(fragment_shader_id, GL_INFO_LOG_LENGTH, &info_log_length);
+    char* info_log = (char*)malloc(info_log_length);
+    glGetShaderInfoLog(fragment_shader_id, info_log_length, NULL, info_log);
+    fprintf(stderr, "Info Log:\n%s\n", info_log);
+    free(info_log);
+    assert(compiled_ok);
+  }
 
-  // Link it all together; shader_program is global
-  shader_program = glCreateProgram();
-  MSGLError();
-  glAttachShader(shader_program, fs);
-  glAttachShader(shader_program, vs);
-  glLinkProgram(shader_program);
-  MSGLError();
+  GLint linked_ok;
+
+  // Link it all together; shader_program_id is global
+  shader_program_id = glCreateProgram();
+  msglError();
+  glAttachShader(shader_program_id, fragment_shader_id);
+  glAttachShader(shader_program_id, vertex_shader_id);
+  glLinkProgram(shader_program_id);
+  glGetProgramiv(shader_program_id, GL_LINK_STATUS, &linked_ok);
+  if (!linked_ok) {
+    fprintf(stderr, "Linking failed.\n");
+    GLint info_log_length;
+    glGetProgramiv(shader_program_id, GL_INFO_LOG_LENGTH, &info_log_length);
+    char* info_log = (char*)malloc(info_log_length);
+    glGetProgramInfoLog(shader_program_id, info_log_length, NULL, info_log);
+    fprintf(stderr, "%s\n", info_log);
+    free(info_log);
+    assert(linked_ok);
+  }
+
+  msglError();
 }
 
 void InitTriangle() {
@@ -157,32 +187,31 @@ void InitTriangle() {
       {glm::vec3(-0.5f, -0.5f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f)},
   };
 
-  MSGLError();
+  msglError();
 
-  GLuint vbo = 0;
-  glGenBuffers(1, &vbo);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  GLuint vertex_buffer_object_id = 0;
+  glGenBuffers(1, &vertex_buffer_object_id);
+  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object_id);
   // If using just floats
   // glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
   glBufferData(GL_ARRAY_BUFFER, 3 * sizeof(vertex_t), points, GL_STATIC_DRAW);
 
-  MSGLError();
+  msglError();
 
-  // vao is global
-  glGenVertexArrays(1, &vao);
-  assert(vao >= 0);
-  glBindVertexArray(vao);
+  // vertex_array_object_id is global
+  glGenVertexArrays(1, &vertex_array_object_id);
+  assert(vertex_array_object_id >= 0);
+  glBindVertexArray(vertex_array_object_id);
   // Vanilla
   // glEnableVertexAttribArray(0);
   // Fancy
-  GLint loc = glGetAttribLocation(shader_program, "in_vertex");
-  MSGLError();
-  printf("in_vertex %d\n", loc);
+  GLint loc = glGetAttribLocation(shader_program_id, "in_vertex");
+  msglError();
   glEnableVertexAttribArray(loc);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object_id);
   // If using just floats
   // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-  // assert(shader_program >= 0);
+  // assert(shader_program_id >= 0);
   // printf("%u\n", loc);
   // Vanilla
   // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertex_t),
@@ -190,77 +219,33 @@ void InitTriangle() {
   // Fancy
   glVertexAttribPointer(loc, 3, GL_FLOAT, GL_FALSE, sizeof(vertex_t),
                         (GLvoid*)offsetof(vertex_t, coord));
-  MSGLError();
+  msglError();
 
-  loc = glGetAttribLocation(shader_program, "in_color");
-  MSGLError();
-  printf("in_color %d\n", loc);
+  loc = glGetAttribLocation(shader_program_id, "in_color");
+  msglError();
   glEnableVertexAttribArray(loc);
-  MSGLError();
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  MSGLError();
+  msglError();
+  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object_id);
+  msglError();
   glVertexAttribPointer(loc, 3, GL_FLOAT, GL_FALSE, sizeof(vertex_t),
                         (GLvoid*)offsetof(vertex_t, color));
-  MSGLError();
-  // loc = glGetAttribLocation(shader_program, "junk");
+  msglError();
+  // loc = glGetAttribLocation(shader_program_id, "junk");
   // printf("junk %d\n", loc);
   // You could assert(loc >= 0);
-  // MSGLError();
+  // msglError();
 }
 
 void DrawTriangle() {
-  MSGLError();
-  glBindVertexArray(vao);
+  msglError();
+  glBindVertexArray(vertex_array_object_id);
   glDrawArrays(GL_TRIANGLES, 0, 3);
-  MSGLError();
-}
-
-void DrawTetrahedron() {
-  glBegin(GL_TRIANGLE_STRIP);
-  glColor3f(1.0F, 1.0F, 1.0F);
-  glVertex3f(0.0F, 1.0F, 0.0F);  // a
-  glColor3f(1.0F, 0.0F, 0.0F);
-  glVertex3f(-1.0F, 0.0F, 1.0F);  // b
-  glColor3f(0.0F, 1.0F, 0.0F);
-  glVertex3f(1.0F, 0.0F, 1.0F);  // c
-  glColor3f(0.0F, 0.0F, 1.0F);
-  glVertex3f(0.0F, 0.0F, -1.0F);  // d
-  glColor3f(1.0F, 1.0F, 1.0F);
-  glVertex3f(0.0F, 1.0F, 0.0F);  // a
-  glColor3f(1.0F, 0.0F, 0.0F);
-  glVertex3f(-1.0F, 0.0F, 1.0F);  // b
-  glEnd();
-
-  glBegin(GL_LINES);
-  glColor3f(0.0F, 0.0F, 0.0F);
-
-  // a b
-  glVertex3f(0.0F, 1.0F, 0.0F);
-  glVertex3f(-1.0F, 0.0F, 1.0F);
-  // a c
-  glVertex3f(0.0F, 1.0F, 0.0F);
-  glVertex3f(1.0F, 0.0F, 1.0F);
-
-  // b c
-  glVertex3f(-1.0F, 0.0F, 1.0F);
-  glVertex3f(1.0F, 0.0F, 1.0F);
-
-  // a d
-  glVertex3f(0.0F, 1.0F, 0.0F);
-  glVertex3f(0.0F, 0.0F, -1.0F);
-  // b d
-  glVertex3f(-1.0F, 0.0F, 1.0F);
-  glVertex3f(0.0F, 0.0F, -1.0F);
-  // c d
-  glVertex3f(1.0F, 0.0F, 1.0F);
-  glVertex3f(0.0F, 0.0F, -1.0F);
-
-  glEnd();
+  msglError();
 }
 
 int main(int argc, char const* argv[]) {
   GLFWwindow* window{nullptr};
-  glfwSetErrorCallback(ErrorCallback);
+  glfwSetErrorCallback(msglfwErrorCallback);
   if (glfwInit() == 0) {
     return 1;
   }
@@ -287,36 +272,31 @@ int main(int argc, char const* argv[]) {
     return 1;
   }
 
-  glfwSetKeyCallback(window, KeyCallback);
-  glfwSetMouseButtonCallback(window, MouseButtonCallback);
-  // glfwSetCursorPosCallback(window, MouseCursorPositionCallback);
-  // glfwSetCursorEnterCallback(window, CursorEnteredCallback);
+  glfwSetKeyCallback(window, msglfwKeyCallback);
+  glfwSetMouseButtonCallback(window, msglfwMouseButtonCallback);
 
-  glClearColor(0.0F, 0.0F, 0.0F, 1.0F);
+  Init();
+
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
   glEnable(GL_DEPTH_TEST);
   glDepthFunc(GL_LESS);
 
-  MSGLError();
-
   InitShader();
-
-  MSGLVersion();
 
   InitTriangle();
 
-  MSGLError();
-
   GLuint u_model_view_matrix =
-      glGetUniformLocation(shader_program, "model_view_matrix");
+      glGetUniformLocation(shader_program_id, "model_view_matrix");
   assert(u_model_view_matrix >= 0);
 
-  MSGLError();
-
   GLuint u_projection_matrix =
-      glGetUniformLocation(shader_program, "projection_matrix");
+      glGetUniformLocation(shader_program_id, "projection_matrix");
   assert(u_projection_matrix >= 0);
 
-  MSGLError();
+  msglError();
+
+  double previous_time{0.0};
+  double z_rotate_angle{0.0};
 
   while (glfwWindowShouldClose(window) == 0) {
     float ratio{NAN};
@@ -325,63 +305,54 @@ int main(int argc, char const* argv[]) {
     glfwGetFramebufferSize(window, &width, &height);
     ratio = width / static_cast<float>(height);
 
-    MSGLError();
-
     glViewport(0, 0, width, height);
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
     glm::mat4 projection_matrix =
         glm::perspective<float>(90.0, ratio, 0.5, 5.0);
-    // lookAt(eye_position, center, up)
-    // glm::mat4 look_at_matrix =
-    //     glm::lookAt(glm::vec3(0.0, 0.0, -2.5), glm::vec3(0.0, 0.0, 0.0),
-    //                 glm::vec3(0.0, 1.0, 0.0));
+
+    glm::mat4 model_view_matrix;
+
+    // glm::lookAt(eye_position, center, up)
     glm::mat4 look_at_matrix =
         glm::lookAt(glm::vec3(0.0, 0.0, -1.0), glm::vec3(0.0, 0.0, 0.0),
                     glm::vec3(0.0, 1.0, 0.0));
-    // glm::mat4 z_rotate = glm::rotate<float>(look_at_matrix, (glfwGetTime() * 50.0F), glm::vec3(0.0f, 0.0f, 1.0f));
-    // glm::mat4 y_rotate = glm::rotate<float>(z_rotate, (glfwGetTime() * 30.0F), glm::vec3(0.0f, 1.0f, 0.0f));
-    // glm::mat4 x_rotate = glm::rotate<float>(y_rotate, (glfwGetTime() * 15.0F), glm::vec3(1.0f, 0.0f, 0.0f));
 
-    glm::mat4 model_view_matrix = look_at_matrix;
-    // glm::mat4 model_view_matrix = x_rotate;
+    double current_time = glfwGetTime();
+    assert(current_time > 0.0);
+    if (rotate_on) {
+      // glfwGetTime() returns time in seconds
+      double elapsed_time = current_time - previous_time;
+      // rotate slowly around the z-axis once every 10 seconds
+      // glm uses radians
+      double previous_angle_degrees = z_rotate_angle * (180.0 / M_PI);
+      double z_rotate_angle_degrees =
+          previous_angle_degrees + (elapsed_time / 10.0) * 360.0;
+      if (z_rotate_angle_degrees > 360.0) {
+        z_rotate_angle_degrees = z_rotate_angle_degrees - 360.0;
+      }
+      z_rotate_angle = z_rotate_angle_degrees * (M_PI / 180.0);
+    }
+    // Rotated around glm::vec3(0.0f, 0.0f, -1.0f) by z_rotate_angle so
+    // the triangle rotates counter clockwise from the camera's perspective.
+    model_view_matrix = glm::rotate<float>(look_at_matrix, z_rotate_angle,
+                                           glm::vec3(0.0f, 0.0f, -1.0f));
 
-    MSGLError();
+    previous_time = current_time;
 
     // Activate our shading program
-    glUseProgram(shader_program);
-
-    MSGLError();
+    glUseProgram(shader_program_id);
 
     // Update any uniform variables
     glUniformMatrix4fv(u_model_view_matrix, 1, GL_FALSE,
                        glm::value_ptr(model_view_matrix));
 
-    MSGLError();
-
     glUniformMatrix4fv(u_projection_matrix, 1, GL_FALSE,
                        glm::value_ptr(projection_matrix));
 
-    MSGLError();
-
-    // glMatrixMode(GL_PROJECTION);
-    // glLoadIdentity();
-    // glFrustum(-ratio, ratio, -ratio, ratio, 1.0F, 40.0F);
-    // MSGLError();
-    //
-    // glMatrixMode(GL_MODELVIEW);
-    // glLoadIdentity();
-    // MSGLError();
-    // gluLookAt(0.0F, 0.0F, -2.5F, 0.0F, 0.0F, 0.0F, 0.0F, 1.0F, 0.0F);
-    // glRotatef(static_cast<float>(glfwGetTime()) * 50.0F, 0.0F, 0.0F, 1.0F);
-    // glRotatef(static_cast<float>(glfwGetTime()) * 30.0F, 0.0F, 1.0F, 0.0F);
-    // glRotatef(static_cast<float>(glfwGetTime()) * 15.0F, 1.0F, 0.0F, 0.0F);
-    // MSGLError();
-
-    // Draw!
-    // DrawTetrahedron();
     DrawTriangle();
-    MSGLError();
+    msglError();
 
     glfwSwapBuffers(window);
     glfwPollEvents();
